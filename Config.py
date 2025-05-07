ed09b0f975d36801e2d07fc481f48e40df2e740d
chenuka <chenukadanith@gmai.com>
2025-05-07 19:11:12 +0530

    2 nd commit

--- Config.py
+++
@@ -1,6 +0,0 @@
-class TestConfig:
-    BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    USERNAME = "Admin"
-    PASSWORD = "admin123"
-    WAIT_TIME = 10
-    SLEEP_TIME = 2
